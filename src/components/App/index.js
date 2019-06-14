f7172f4b3174412b2f039fbde0463313ec7aeebd
Brennan <brennanduffey@gmail.com>
2019-06-14 10:13:14 -0600

    Start routes for sidebar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    main: () => {Home}
+  },
+  {
+    path: '/About',
+    exact: true,
+    main: () => {About}
+  },
+  {
+    path: '/Portfolio',
+    exact: true,
+    main: () => {Portfolio}
+  },
+  {
+    path: '/Contact',
+    exact: true,
+    main: () => {Contact}
+  }
+]
 
 function App() {
   return (
